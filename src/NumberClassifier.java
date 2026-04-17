d9cdbf43e07774a0be131ad4fc1c43fc4e19c645
kristina-pachi <kristina.pachi.04@bk.ru>
2026-04-17 19:30:43 +0300

    Задание 1.1: if/else — Классификация числа

---
+++ src/NumberClassifier.java
@@ -0,0 +1,10 @@
+public class NumberClassifier {
+    public static String classify(int number) {
+        if (number < 0) return "-> отрицательное";
+        if (number == 0) return "-> ноль";
+        if (number < 10) return "-> однозначное";
+        if (number < 100) return "-> двузначное";
+        if (number < 1000) return "-> трёхзначное";
+        return "-> большое";
+    }
+}
