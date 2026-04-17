65d407ae91c99f90686f686f7da9cc45709839c1
kristina-pachi <kristina.pachi.04@bk.ru>
2026-04-17 19:36:05 +0300

    Задание 1.1: add to main

--- src/main.java
+++ src/main.java
@@ -1,2 +1,7 @@
-public class main {
+public static void main(String[] args) {
+    int[] numbers = {-5, 0, 7, 42, 100, 1000, -999};
+
+    for (int n : numbers) {
+        System.out.println(n + " " + NumberClassifier.classify(n));
+    }
 }
